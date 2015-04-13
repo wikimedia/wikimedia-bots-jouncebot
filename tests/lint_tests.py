42607bf3288dfe0c0af595adf24b88f99604e95c
Bryan Davis <bd808@wikimedia.org>
2015-04-13 15:05:09 -0600

    Use tox lint yaml and run flake8
    Bug: T95894
    Change-Id: Id74f153c940f90450f3db56c87f357074cde15fc

---
+++ tests/lint_tests.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import yaml
+
+
+def lint_yaml(path):
+    try:
+        obj = yaml.safe_load(file(path, 'r'))
+        assert obj, "Parse failed for %s: empty result" % path
+    except yaml.error.YAMLError, exc:
+        assert False, "YAML parse error in %s: %s" % (path, exc)
+
+
+def test_lint_DefaultConfig():
+    lint_yaml(os.path.realpath(
+        os.path.join(os.path.dirname(__file__), '../DefaultConfig.yaml')))
